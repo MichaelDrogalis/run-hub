2cdda3e4e2534d3683bf2c8fed88c6ddf37c7ddb
Michael Drogalis <mjd3089@rit.edu>
2012-08-19 10:58:44 -0400

    Initial commit.

---
+++ test/run_hub/test/handler.clj
@@ -0,0 +1,14 @@
+(ns run-hub.test.handler
+  (:use clojure.test
+        ring.mock.request  
+        run-hub.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+  
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
